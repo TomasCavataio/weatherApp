b7d4d41a57254c54ef9e44f3a86f5a2dc513345c
Tomas Cavataio <tomas.cavataio@ext.essilor.com>
2023-10-12 21:58:44 +0200

    Fix PWA issues & prepare github deoployment

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,17 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/ngsw-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registrado con éxito:', registration);
+    })
+    .catch((error) => {
+      console.error('Error al registrar el Service Worker:', error);
+    });
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
